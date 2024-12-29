1e5f0d840039690462a8bfae41466c0c72fca64b
caiosfg <caio.sfg@gmail.com>
2024-12-29 01:38:01 -0300

    pass to version

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\V1\SatelliteController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/satellite', SatelliteController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
 });
 
 Route::get('/user', function (Request $request) {
